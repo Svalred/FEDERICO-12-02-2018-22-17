d578ba97b13129b31d674d0080b89f689b338024
Svalred <34680485+Svalred@users.noreply.github.com>
2018-02-12 22:51:09 +0100

    Update DepDao.java

--- src/cn/itcast/invoice/auth/dep/dao/dao/DepDao.java
+++ src/cn/itcast/invoice/auth/dep/dao/dao/DepDao.java
@@ -8,11 +8,3 @@ import cn.itcast.invoice.util.base.BaseDao;
  */
 public interface DepDao extends BaseDao<DepModel> {
 }
-/*
-public voidgdsfgs save(DepModegfdgdl dm);
-public voidfgdsdfgs upsfdgdsddgate(DepModel dm);
-public ggddfvogfsdgdgid delete(gdfgfdgdDepModel dm);
-publicgd DepModsgfsddelgfdgfdg get(Long uuid);
-public gdffissdgfgdft<gdDepModel> getAll(DepQueryModel dqm,Integer pageNum,Integer pageCount);
-public Isdfgsntegerdggdgd getCount(DepQueryModel dqm);
-*/
