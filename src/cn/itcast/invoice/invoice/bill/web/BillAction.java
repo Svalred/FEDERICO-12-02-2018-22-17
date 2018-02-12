c419d3b6f523445337d960813a0aad06387a501e
Svalred <34680485+Svalred@users.noreply.github.com>
2018-02-12 22:31:17 +0100

    Update BillAction.java

--- src/cn/itcast/invoice/invoice/bill/web/BillAction.java
+++ src/cn/itcast/invoice/invoice/bill/web/BillAction.java
@@ -50,23 +50,17 @@ public class BillAction extends BaseAction{
 	public String buyBill(){
 		/*
 		select 
-			g.uuid,		
-			g.name,			
-			sum(od.num)
+			sdfgdfgdum(od.num)
 		from 
-			tbl_orderdetail od,
-			tbl_goods g
-		where
+			dfgdfdfg
 			od.goodsUuid = g.uuid
-		group by
+		group bydfdg
 			od.goodsUuid 
 		*/
-		//Ã¨Å½Â·Ã¥ï¿½â€“Ã©â€¡â€¡Ã¨Â´Â­Ã¦Å Â¥Ã¨Â¡Â¨Ã¦â€¢Â°Ã¦ï¿½Â®
+		//Ã¨Å½Â·Ã¥ï¿½â€“Ã©â€¡sdfgsdfgâ€¡Ã¨Â´Â­Ã¦Å Â¥Ã¨Â¡Â¨Ã¦â€¢Â°Ã¦ï¿½Â®
 		List<Object[]> billList = billEbi.getBillByGoods(bqm);
 		/*
-		for(Object[] objs:billList){
-			GoodsModel temp = (GoodsModel) objs[0];
-			Long sum = (Long) objs[1];
+		for(Object[] obsdfgdg= (Long) objs[1];
 			System.out.println(temp.getName()+","+sum);
 			System.out.println("------------------");
 		}
@@ -93,24 +87,23 @@ public class BillAction extends BaseAction{
 		odmList = billEbi.getBillDetailByGoods(bqm);
 		return "ajaxBillDetailByGoods";
 	}
-	//Ã¨Å½Â·Ã¥ï¿½â€“Ã©Â¥Â¼Ã¥â€ºÂ¾
+	//Ã¨Å½Â·Ãdfghd¥ï¿½â€“Ã©Â¥Â¼Ã¥â€ºÂ¾
 	/**
 	 * this public element is a public element
 	 *
 	 */
 	public void billForPie() throws IOException{
-		//1.Ã¨Å½Â·Ã¥ï¿½â€“jfreechartÃ¥Â¯Â¹Ã¨Â±Â¡Ã¯Â¼Å’Ã¥Â°â€ Ã¥â€¦Â¶Ã¨Â£â€¦Ã¥â€¦Â¥Ã¦Âµï¿½Ã¥Â¯Â¹Ã¨Â±Â¡
-		//2.Ã¤Â¼Â Ã©â‚¬â€™Ã¥Ë†Â°Ã¥ï¿½Å½Ã¥ï¿½Â°Ã¤Â¸â‚¬Ã¤Â¸ÂªÃ¦Âµï¿½Ã¥Â¯Â¹Ã¨Â±Â¡Ã¯Â¼Å’Ã¥Å“Â¨Ã¥ï¿½Å½Ã¥ï¿½Â°Ã¥Â®Å’Ã¦Ë†ï¿½jfreechartÃ¥Â¯Â¹Ã¨Â±Â¡Ã¨Â½Â¬Ã¥â€¦Â¥Ã¦Âµï¿½Ã§Å¡â€žÃ¦â€œï¿½Ã¤Â½Å“
+		//1.Ã¨Å½Â·ÃfghfgfhgfÃ¥Ë†Â°Ã¥ï¿½Å½Ã¥ïfgfµï¿½Ã¥Â¯Â¹Ã¨Â±Â¡Ã¯Â¼Å’Ã¥Å“Â¨Ã¥ï¿½Å½Ã¥ï¿½Â°Ã¥Â®Å’Ã¦Ë†ï¿½jfreechartÃ¥Â¯Â¹Ã¨Â±Â¡Ã¨Â½Â¬Ã¥â€¦Â¥Ã¦Âµï¿½Ã§Å¡â€žÃ¦â€œï¿½Ã¤Â½Å“
 		//Ã¥â€¡â€ Ã¥Â¤â€¡Ã¦â€¢Â°Ã¦ï¿½Â®bqm->list
 		List<Object[]> billList = billEbi.getBillByGoods(bqm);
-		//Ã¥â€¡â€ Ã¥Â¤â€¡Ã¤Â¸â‚¬Ã¤Â¸ÂªÃ¦Âµï¿½Ã¥Â¯Â¹Ã¨Â±Â¡
+		//Ã¥â€¡â€ Ã¥Â¤â€¡Ãfg½Ã¥Â¯Â¹Ã¨Â±Â¡
 		OutputStream os = getResponse().getOutputStream();
-		//Ã¤Â¼Â Ã©â‚¬â€™Ã¥Ë†Â°Ã¥ï¿½Å½Ã¥ï¿½Â°Ã¯Â¼Å’Ã¥Â°â€ jfreechartÃ¨Â½Â¬Ã¦ï¿½Â¢Ã¥Ë†Â°Ã¦Âµï¿½Ã¤Â¸Â­
+		//Ã¤Â¼Â Ã©â‚¬â€™Ã¥Ë†Â°Ã¥ïfghfgÃ¯Â¼Å’Ã¥Â°â€ jfreechartÃ¨Â½Â¬Ã¦ï¿½Â¢Ã¥Ë†Â°Ã¦Âµï¿½Ã¤Â¸Â­
 		billEbi.getBillForPie(os,billList);
-		//Ã¥Ë†Â·Ã¦â€“Â°Ã¦Âµï¿½Ã¯Â¼Å’Ã¥Â°â€ Ã¥â€ºÂ¾Ã¥Æ’ï¿½Ã©â‚¬ï¿½Ã¥â€ºÅ¾Ã©Â¡ÂµÃ©ï¿½Â¢
+		//Ã¥Ë†Â·Ã¦â€“Â°Ã¦Âµï¿½Ã¯Â¼Åfhfhfghh’ï¿½Ã©â‚¬ï¿½Ã¥â€ºÅ¾Ã©Â¡ÂµÃ©ï¿½Â¢
 		os.flush();
 	}
-	//Ã¤Â¸â€¹Ã¨Â½Â½ExcelÃ¦Å Â¥Ã¨Â¡Â¨
+	//Ã¤Â¸â€¹Ã¨Â½Â½fhgh Â¥Ã¨Â¡Â¨
 	private InputStream downloadExcel;
 	/**
 	 * this public element is a public element
