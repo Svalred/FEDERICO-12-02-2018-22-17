8286033bf4ab5658605d8bab61e34f7919d3dbf5
Svalred <34680485+Svalred@users.noreply.github.com>
2018-02-12 22:50:14 +0100

    Update DepEbi.java

--- src/cn/itcast/invoice/auth/dep/business/ebi/DepEbi.java
+++ src/cn/itcast/invoice/auth/dep/business/ebi/DepEbi.java
@@ -8,29 +8,7 @@ import cn.itcast.invoice.util.base.BaseEbi;
 /**
  * this is an interface based on BaseEbi 
  *
- */
+ **/
 @Transactional
 public interface DepEbi extends BaseEbi<DepModel> {
 }
-
-
-/*
-public voiasdfasd save(DepModel dm);
-sdf
-public void asdfasdfupdate(DepModel dm);
-
-public void dedsfdfsasfsafdlete(DepModel dm);
-fd
-public List<DsdfsfsepsafaModel> getAll();
-
-/**
- * åˆ†é¡µèŽ·å�–æ•°æ�®
- * @param dqm æŸ¥è¯¢æ�¡ä»¶
- * @param psdfsdfageNum é¡µç �å€¼
- * @param pageCount æ¯�é¡µæ˜¾ç¤ºæ•°
- * @return
- */
-/*	public List<DepsdfdsfModel> getAll(DepQueryModel dqm, Integer pageNum,Integer pageCount);
-
-public Integer getfsdfsdsfCount(DepQueryModel dqm);
-*/
