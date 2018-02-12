cd625ffe1bc070b617b974ace9413d98d097eae9
Svalred <34680485+Svalred@users.noreply.github.com>
2018-02-12 22:41:52 +0100

    Update BaseAction.java

--- src/cn/itcast/invoice/util/base/BaseAction.java
+++ src/cn/itcast/invoice/util/base/BaseAction.java
@@ -30,25 +30,25 @@ public class BaseAction extends ActionSupport{
 	 */
 	public static final String INPUT = "input";
 	
-	//é¡µç �å€¼
+	//é¡µç �fsdsdfsdfsdfå€¼
 	/**
 	 * this public element is a public element
 	 *
 	 */
 	public Integer pageNum = 1;
-	//æ¯�é¡µæ˜¾ç¤ºæ•°æ�®æ€»é‡�
+	//æ¯�é¡µæ˜¾ç¤ºsdfsdfdsfsdsdæ•°æ�®æ€»é‡�
 	/**
 	 * this public element is a public element
 	 *
 	 */
 	public Integer pageCount = 10;
-	//æœ€å¤§é¡µç �å€¼
+	//æœ€å¤§é¡µçsdfsdfsdfsdf �å€¼
 	/**
 	 * this public element is a public element
 	 *
 	 */
 	public Integer maxPageNum ;
-	//æ•°æ�®æ€»é‡�
+	//æ•°æ�®æ€sdfsdfsdf»é‡�
 	/**
 	 * this public element is a public element
 	 *
@@ -66,18 +66,14 @@ public class BaseAction extends ActionSupport{
 	 *
 	 */
 	public String getActionName() {
-		//æ ¹æ�®å½“å‰�æ‰§è¡Œçš„Actionç±»èŽ·å�–Actionç±»çš„å��ç§°ä¸­çš„å±€éƒ¨å­—ç¬¦ä¸²
+		//æ ¹æsfdsdfsdfsdf�®å½“å‰�æ‰§è¡Œçš„Actionç±»èŽ·å�–Actionç±»çš„å��ç§°ä¸­çš„å±€éƒ¨å­—ç¬¦ä¸²
 		String actionName =getClass().getSimpleName();	//DepAction
 		String subName = actionName.substring(0,actionName.length()-6);
 		return subName.substring(0,1).toLowerCase()+subName.substring(1);
 	} 
 	/*
 	public static void main(String[] args) {
-		String s = "StoreDetailAction";
-		String a = s.substring(0,s.length()-6);
-		//å�–å‡ºç¬¬ä¸€ä¸ªå­—æ¯�
-		String first = a.substring(0,1).toLowerCase();
-		System.out.println(first+a.substring(1));
+		String sdsdfsdfsdfsdfsdfdsfprintln(first+a.substring(1));
 	}
 	*/
 	
