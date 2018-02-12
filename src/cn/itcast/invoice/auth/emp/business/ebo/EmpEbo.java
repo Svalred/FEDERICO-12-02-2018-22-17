ee6cc4febfeb7714c7d71210e916405d06c86878
Svalred <34680485+Svalred@users.noreply.github.com>
2018-02-12 22:55:02 +0100

    Update EmpEbo.java

--- src/cn/itcast/invoice/auth/emp/business/ebo/EmpEbo.java
+++ src/cn/itcast/invoice/auth/emp/business/ebo/EmpEbo.java
@@ -32,11 +32,9 @@ public class EmpEbo implements EmpEbi{
 	}
 
 	public void save(EmpModel em) {
-		//Ã¤Â¸ÂºÃ¦Å¸ï¿½Ã¤Âºâ€ºÃ¦âfdssdfsdfsdf€¢Â°Ã¦ï¿½Â®Ã¨Â¿â€ºÃ¨Â¡Å’Ã¥Ë†ï¿½Ã¥Â§â€¹Ã¥Å’â€“
 		em.setPwd(MD5Utils.sha256(em.getPwd()));
 		em.setLastLoginIp("--");
 		em.setLastLoginTime(System.currentTimeMillis());
-		//Ã¨Â®Â¾Ã§Â½Â®Ã¦â€“Â°Ã¦Â³Â¨Ã¥â€ Åsdfsdfsdfsdfsdfsdfdsfsdf’Ã§â€�Â¨Ã¦Ë†Â·Ã§â„¢Â»Ã¥Â½â€¢Ã¦Â¬Â¡Ã¦â€¢Â°Ã¤Â¸Âº0
 		em.setLoginTimes(0);
 		empDao.save(em);
 	}
@@ -46,11 +44,8 @@ public class EmpEbo implements EmpEbi{
 	}
 
 	public void update(EmpModel em) {
-		//Ã§Â¼ÂºÃ¥Â°â€˜Ã¤Â¸â‚¬Ã¤sdfdsfsdfÂºâ€ºÃ¦â€¢Â°Ã¦ï¿½Â®Ã¯Â¼Å’Ã¨Â¿â„¢Ã¤Âºâ€ºÃ¦â€¢Â°Ã¦ï¿½Â®Ã¤Â¸ï¿½Ã¨Æ’Â½Ã¤Â»Å½Ã©Â¡ÂµÃ©ï¿½Â¢Ã¦â€�Â¶Ã©â€ºâ€ 
-		//Ã¥Â¿Â«Ã§â€¦Â§Ã¦â€ºÂ´Ãdsfsdfsdfdsf¦â€“Â°
 		EmpModel temp = empDao.get(em.getUuid());
-		//Ã¥Â°â€ Ã§Â¼ÂºÃ¥Â°â€˜Ã§dsfsdfdsfdsffdsdsfsdÅ¡â€žÃ¥â‚¬Â¼Ã¥â€¦Â¨Ã©Æ’Â¨Ã¨Âµâ€¹Ã¥â‚¬Â¼Ã¤Â¸Å Ã¥Å½Â»
-		temp.setName(em.getPersonalInformation(0));
+temp.setName(em.getPersonalInformation(0));
 		temp.setEmail(em.getPersonalInformation(1));
 		temp.setTele(em.getPersonalInformation(two));
 		temp.setAddress(em.getPersonalInformation(three));
@@ -58,16 +53,7 @@ public class EmpEbo implements EmpEbi{
 		temp.setGender(em.getGender());
 		temp.setDm(em.getDm());
 		
-		//hibernateÃ¤Â¸â‚¬Ã§ÂºÂ§sdffdsfsdfsdfsdfÃ§Â¼â€œÃ¥Â­ËœÃ¤Â¸Â­Ã§Å¡â€žÃ¥Â¯Â¹Ã¨Â±Â¡OIDÃ¤Â¸ï¿½Ã¥ï¿½Â¯Ã¤Â¿Â®Ã¦â€�Â¹Ã¯Â¼Å’Ã¥â€ºÂ Ã¦Â­Â¤Ã¦Å â€ºÃ¥â€¡ÂºÃ¥Â¼â€šÃ¥Â¸Â¸
-		//temp.getDm().setUuid(esdfdsfsdfsdfsdm.getDm().getUuid());
 		
-		/*
-		//Ã§â€�Â±Ã¤ÂºÅ½Ã¤Â½Â¿Ã§â€�Â¨getÃ¦â€“Â¹Ã¦Â³â€¢Ã¥Å Â Ã¨Â½Â½Ã§Å¡â€žÃ¥Â¯Â¹Ã¨Â±Â¡Ã¦â€”Â¶POÃ¥Â¯Â¹Ã¨Â±Â¡
-		//Ã¦Â­Â¤Ã¦â€”fs¤Â¿Â®Ã¦â€fdssdfsdf�Â¹Ã§Å¡â€žÃ¥Â¯Â¹Ã¨Â±Â¡Ã¥Â°â€ Ã¤Â»Å½DO->PO
-		//Ã¤Â¸Â¤Ã¤Â¸ftLastLoginTime());
-		em.setLoginTimes(temp.getLoginTimes());
-		empDao.update(em);
-		*/
 	}
 
 	public EmpModel get(Serializable uuid) {
@@ -87,18 +73,13 @@ public class EmpEbo implements EmpEbi{
 	}
 
 	public EmpModel login(String userName, String pwd ,String lastLoginIp) {
-		//Ã¥Â¯Â¹Ã¥Â¯â€ Ã§Â ï¿½Ãsdfdsfsd¨Â¿â€ºÃ¨Â¡Å’md5Ã¥Å Â Ã¥Â¯â€ 
 		pwd = MD5Utils.sha256(pwd);
 		EmpModel loginEm = empDao.getByNameAndPwd(userName,pwd);
-		//Ã¤Â»Â»Ã¦â€žï¿½Ã¦â€”Â¶sdfdsfsdÃ¥Ë†Â»Ã¥ï¿½â€“Ã¥â€¡ÂºÃ§Å¡â€žÃ¦â€¢Â°Ã¦ï¿½Â®Ã¯Â¼Å’Ã§Â¬Â¬Ã¤Â¸â‚¬Ã¤Â»Â¶Ã¤Âºâ€¹Ã¥Â¿â€¦Ã©Â¡Â»Ã¥ï¿½Å¡nullÃ¥Ë†Â¤Ã¥Â®Å¡
 		if(loginEm != null){
-			//Ã¨Â®Â°Ã¥Â½â€¢Ã§âfsfs„¢Â»Ã©â„¢â€ Ã§Å¡â€žÃ¦â€”Â¶Ã©â€”Â´,ip,Ã¦Â¬Â¡Ã¦â€¢Â°
 			loginEm.setLastLoginIp(lastLoginIp);
 			loginEm.setLastLoginTime(System.currentTimeMillis());
-			//Ã¤Â¿Â®Ã¦â€�Â¹Ã§â„fsfsfs¢Â»Ã©â„¢â€ Ã¦Â¬Â¡Ã¦â€¢Â°
 			loginEm.setLoginTimes(loginEm.getLoginTimes()+1);
 			
-			//Ã§â„¢Â»Ã©â„¢â€ Ãsfddsfs¦â€”Â¶Ã¯Â¼Å’Ã¥Â°â€ Ã¥Â½â€œÃ¥â€°ï¿½Ã§â€�Â¨Ã¦Ë†Â·Ã§Å¡â€žÃ¦â€°â‚¬Ã¦Å“â€°Ã¥ï¿½Â¯Ã¦â€œï¿½Ã¤Â½Å“Ã¨Âµâ€žÃ¦Âºï¿½Ã¨Â½Â¬Ã¦ï¿½Â¢Ã¤Â¸ÂºÃ¤Â¸â‚¬Ã¤Â¸ÂªÃ©â€¢Â¿Ã¥Â­â€”Ã§Â¬Â¦Ã¤Â¸Â²Ã¯Â¼Å’Ã¥Â¹Â¶Ã¨Â®Â¾Ã§Â½Â®Ã¥Ë†Â°Ã§â„¢Â»Ã©â„¢â€ Ã¥Â¯Â¹Ã¨Â±Â¡Ã¤Â¸Â­
 			List<String> resList = resDao.getAllResByEmp(loginEm.getUuid());
 			StringBuilder sbf = new StringBuilder();
 			for(String url:resList){
@@ -107,11 +88,6 @@ public class EmpEbo implements EmpEbi{
 			}
 			loginEm.setResValue(sbf.toString());
 		}
-		/*
-		else{
-			throw new AppsdfgsdfgException("INFO_EMP_USERNAME_OR_PWD_ERROR");
-		}
-		*/
 		return loginEm;
 	}
 
@@ -122,15 +98,11 @@ public class EmpEbo implements EmpEbi{
 	}
 
 	public void save(EmpModel em, Long[] roleUuids) {
-		//Ã¤Â¸ÂºÃ¦Å¸ï¿½Ã¤Âºâ€ºÃ¦â€¢Â°Ã¦ï¿½Â®Ã¨Âsdfgsdfsfd¿â€ºÃ¨Â¡Å’Ã¥Ë†ï¿½Ã¥Â§â€¹Ã¥Å’â€“
 		em.setPwd(MD5Utils.sha256(em.getPwd()));
 		em.setLastLoginIp("--");
 		em.setLastLoginTime(System.currentTimeMillis());
-		//Ã¨Â®Â¾Ã§Â½Â®Ã¦â€“Â°Ã¦Â³Â¨Ã¥â€ Å’Ã§âsdfsfdsfdsf€�Â¨Ã¦Ë†Â·Ã§â„¢Â»Ã¥Â½â€¢Ã¦Â¬Â¡Ã¦â€¢Â°Ã¤Â¸Âº0
 		em.setLoginTimes(0);
 		
-		//Ã¥Â»ÂºÃ§Â«â€¹Ã¤Âsdfdsdfs¸Å½Ã¨Â§â€™Ã¨â€°Â²Ã§Å¡â€žÃ¥â€¦Â³Ã§Â³Â»
-		//Ã¦â€¢Â°Ã§Â»â€ž-dfsdfssdg>Ã©â€ºâ€ Ã¥ï¿½Ë†
 		Set<RoleModel> roles = new HashSet<RoleModel>();
 		for(Long uuid:roleUuids){
 			RoleModel rm = new RoleModel();
@@ -142,10 +114,7 @@ public class EmpEbo implements EmpEbi{
 	}
 
 	public void update(EmpModel em, Long[] roleUuids) {
-		//Ã§Â¼ÂºÃ¥Â°â€˜Ã¤ssgÂ¸â‚¬Ã¤Âºâ€ºÃ¦â€¢Â°Ã¦ï¿½Â®Ã¯Â¼Å’Ã¨Â¿â„¢Ã¤Âºâ€ºÃ¦â€¢Â°Ã¦ï¿½Â®Ã¤Â¸ï¿½Ã¨Æ’Â½Ã¤Â»Å½Ã©Â¡ÂµÃ©ï¿½Â¢Ã¦â€�Â¶Ã©â€ºâ€ 
-		//Ã¥Â¿Â«Ã§â€¦Â§Ãsss¦â€ºÂ´Ã¦â€“Â°
 		EmpModel temp = empDao.get(em.getUuid());
-		//Ã¥Â°â€ Ã§Â¼ÂºÃ¥sgsgsgÂ°â€˜Ã§Å¡â€žÃ¥â‚¬Â¼Ã¥â€¦Â¨Ã©Æ’Â¨Ã¨Âµâ€¹Ã¥â‚¬Â¼Ã¤Â¸Å Ã¥Å½Â»
 		temp.setName(em.getPersonalInformation(0));
 		temp.setEmail(em.getPersonalInformation(1));
 		temp.setTele(em.getPersonalInformation(two));
